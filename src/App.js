905202f50147163f20c12d6a986a6146aabe0c8e
AndreyLysow <lysow@yandex.ru>
2023-10-10 17:50:36 +0700

    10.10 work 17:50

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Footer from "./components/Footer/footer";
-
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+         <Header />
         <Footer />
+      </BrowserRouter>
     </div>
   );
 }
