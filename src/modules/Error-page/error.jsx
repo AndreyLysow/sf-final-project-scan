9a16c8b6cb411175f6a429806d0571e999d8df9e
AndreyLysow <lysow@yandex.ru>
2023-10-11 11:54:05 +0700

    11.10 work 11:54

--- src/modules/Error-page/error.jsx
+++ src/modules/Error-page/error.jsx
@@ -6,10 +6,19 @@ function Error() {
   return (
     <div className="error">
       <div className="error__code">
+        <div className="error__span-first">
+          <span>4</span>
+        </div>
+        <div className="error__span-second">
+          <span>0</span>
+        </div>
+        <div className="error__span-third">
+          <span>4</span>
+        </div>
       </div>
       <p className="error__info">
-        Страница в разработке <br />
-        Перейдите на <Link to="/"><span>Главную</span></Link>
+        Здесь <span>пока</span> ничего нет <br />
+        Вернитесь на <Link to="/">главную</Link>
       </p>
     </div>
   );
