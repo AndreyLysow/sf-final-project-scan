be63e00898b49367fdbed9d3884a3aad31606854
AndreyLysow <lysow@yandex.ru>
2023-10-11 10:52:21 +0700

    11.10 work 11:10

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./App.css";
 import Header from "./modules/Header/header";
+import Main from "./modules/Main/main";
 import Footer from "./modules/Footer/footer";
-import { BrowserRouter } from "react-router-dom";
+import Error from "./modules/Error-page/error";
+import Authorization from "./modules/Authorization/auth";
+import Search from "./modules/Search/search";
+import SearchResult from "./modules/SearchResult/searchResult";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const routesData = [
+  { path: "*", element: <Main /> },
+  { path: "/error", element: <Error /> },
+  { path: "/auth", element: <Authorization /> },
+  { path: "/search", element: <Search /> },
+];
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-         <Header />
+        <Header />
+        <main>
+          <Routes>
+            {routesData.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </div>
   );
 }
+
 export default App;
+
